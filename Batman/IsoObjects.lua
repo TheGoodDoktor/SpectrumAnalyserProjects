45f5896eb92d4e1c9ba4a4e9727bc063f576669f
TheGoodDoktor <mrmarkcraig@googlemail.com>
2024-01-05 16:49:28 +0000

    Added lua files

---
+++ Batman/IsoObjects.lua
@@ -0,0 +1,53 @@
+
+PlayerIsoObjectA = 0x8f18
+PlayerIsoObjectB = 0x8f2a
+
+function DrawIsoObject(address)
+
+	local ptr1 = ReadWord(address)
+	local ptr2 = ReadWord(address + 2)
+
+	local bits = ReadByte(address + 4)
+	local isoSWNE = ReadByte(address + 5)
+	local isoSENW = ReadByte(address + 6)
+
+	local spriteNo = ReadByte(address + 8)
+
+	imgui.Text("Object: ")
+	DrawAddressLabel(address)
+	imgui.SameLine()
+	imgui.Text("Sprite no: " .. tostring(spriteNo))
+end
+
+function DrawIsoObjectsInfo()
+
+	imgui.Text("Behind")
+	local fPtr = PlayerIsoObjectA
+
+	while fPtr ~= 0 do
+		DrawIsoObject(fPtr)
+		fPtr = ReadWord(fPtr)
+	end
+	
+	imgui.Separator()
+	imgui.Text("In front")
+	local bPtr = PlayerIsoObjectA + 2
+
+	while bPtr ~= 0 do
+		DrawIsoObject(bPtr - 2)
+		bPtr = ReadWord(bPtr)
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
