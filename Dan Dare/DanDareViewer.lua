e6279ffaee86105ccb05ecbb1e13342495ac3940
TheGoodDoktor <TheGoodDoktor@users.noreply.github.com>
2024-06-24 22:48:37 +0100

    Dan Dare WIP - formatting scripts

--- Dan Dare/DanDareViewer.lua
+++ Dan Dare/DanDareViewer.lua
@@ -2,6 +2,37 @@
 
 function FormatRoomMemory()
 
+	local roomAddress = globals.RoomDefinitions
+	local roomNo = 0
+
+	while roomNo < 16 do
+		-- TODO: set data item to 16 bit decimal number
+		local roomSize = ReadWord(roomAddress)
+		SetDataItemComment(roomAddress, "Room " .. tostring(roomNo) .. " size (".. tostring(roomSize) .. " bytes)")
+		roomAddress = roomAddress + 2
+
+		while roomSize > 0 do
+
+			-- TODO: format item to byte
+			SetDataItemComment(roomAddress, "item number")
+			local roomItem = ReadByte(roomAddress)
+			roomAddress = roomAddress + 1
+
+			-- TODO: format item to 16 but number
+			SetDataItemComment(roomAddress, "char map offset")
+			local itemPosition = ReadWord(roomAddress)
+			roomAddress = roomAddress + 2
+
+			roomSize = roomSize - 3
+		end
+
+		roomNo = roomNo + 1
+	end
+
+end
+
+function FormatRoomItemMemory()
+
 end
 
 DanDareViewer = 
@@ -19,7 +50,8 @@ DanDareViewer =
         imgui.Text("This viewer is called: " .. tostring(self.name))
 
 		if imgui.Button("Format Room Memory") == true then
-			FormatRoomMemory();
+			FormatRoomMemory();			
+			FormatRoomItemMemory();
 		end
 
     end
