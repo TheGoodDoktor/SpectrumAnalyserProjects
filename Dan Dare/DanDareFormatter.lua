44e9ec41bd17fc95586024a4c52ce64a59b971e8
TheGoodDoktor <mrmarkcraig@googlemail.com>
2024-06-28 13:34:06 +0100

    Dan Dare - Lua

---
+++ Dan Dare/DanDareFormatter.lua
@@ -0,0 +1,93 @@
+
+function FormatRoomMemory()
+
+	local roomAddress = globals.RoomDefinitions
+	local roomNo = 0
+
+	while roomNo < 256 do
+		-- TODO: set data item to 16 bit decimal number
+		local roomSize = ReadWord(roomAddress)
+		AddCommentBlock(roomAddress, "Room " .. tostring(roomNo) .. " Definition")
+		FormatMemory({StartAddress = roomAddress, DataType = EDataType.Word, ItemSize = 2, DisplayType = EDataItemDisplayType.Decimal})
+		SetDataItemComment(roomAddress, "Room size (".. tostring(roomSize) .. " bytes)")
+		roomAddress = roomAddress + 2
+
+		while roomSize > 0 do
+
+			-- format item to byte
+			FormatMemory({StartAddress = roomAddress, DataType = EDataType.Byte, DisplayType = EDataItemDisplayType.Decimal})
+			SetDataItemComment(roomAddress, "item number")
+			local roomItem = ReadByte(roomAddress)
+			roomAddress = roomAddress + 1
+
+			-- format item to 16 bit number
+			FormatMemory({StartAddress = roomAddress, DataType = EDataType.Word, NoItems = 2, DisplayType = EDataItemDisplayType.Hex})
+			SetDataItemComment(roomAddress, "char map offset")
+			local itemPosition = ReadWord(roomAddress)
+			roomAddress = roomAddress + 2
+
+			roomSize = roomSize - 3
+		end
+
+		roomNo = roomNo + 1
+	end
+
+end
+
+function FormatRoomItemMemory()
+
+	local instanceAddress = globals.RoomInstances
+	local instanceNo = 0
+
+	while instanceNo < 20 do
+
+		local instanceSize = ReadWord(instanceAddress)
+
+		AddCommentBlock(instanceAddress, "Room Instance " .. tostring(instanceNo) .. " Definition")
+		FormatMemory({StartAddress = instanceAddress, DataType = EDataType.Word, ItemSize = 2, DisplayType = EDataItemDisplayType.Decimal})
+		SetDataItemComment(instanceAddress, "Instance size (".. tostring(instanceSize) .. " bytes)")
+		instanceAddress = instanceAddress + 2
+
+		local charLength = 0
+		local charStartAddress = 0
+
+		while instanceSize > 0 do
+
+			local val = ReadByte(instanceAddress)
+			instanceAddress = instanceAddress + 1
+			instanceSize = instanceSize - 1
+	
+			if val == 0 then
+
+				-- format character map	
+				FormatMemoryAsCharMap(charStartAddress,charLength,1,globals.BackgroundCharacters)
+				charLength = 0
+				charStartAddress = 0
+
+				-- TODO: format gap
+				FormatMemory({StartAddress = instanceAddress-1, DataType = EDataType.ByteArray, DisplayType = EDataItemDisplayType.Decimal, ItemSize = 2})
+				local gapLength = ReadByte(instanceAddress)
+				SetDataItemComment(instanceAddress-1, "space " .. tostring(gapLength) .. " chars")
+
+				instanceAddress = instanceAddress + 1
+				instanceSize = instanceSize - 1
+	
+			else
+				if charLength == 0 then
+					charStartAddress = instanceAddress - 1
+				end
+	
+				charLength = charLength + 1
+			end
+		end
+
+		-- do last chars
+		if charLength > 0 then
+			FormatMemoryAsCharMap(charStartAddress,charLength,1,globals.BackgroundCharacters)
+		end
+
+		instanceNo = instanceNo + 1
+		
+	end
+
+end
--- Dan Dare/DanDareViewer.lua
+++ Dan Dare/DanDareViewer.lua
@@ -1,96 +1,71 @@
 -- Basic viewer template
 
-function FormatRoomMemory()
-
-	local roomAddress = globals.RoomDefinitions
-	local roomNo = 0
-
-	while roomNo < 256 do
-		-- TODO: set data item to 16 bit decimal number
-		local roomSize = ReadWord(roomAddress)
-		AddCommentBlock(roomAddress, "Room " .. tostring(roomNo) .. " Definition")
-		FormatMemory({StartAddress = roomAddress, DataType = EDataType.Word, ItemSize = 2, DisplayType = EDataItemDisplayType.Decimal})
-		SetDataItemComment(roomAddress, "Room size (".. tostring(roomSize) .. " bytes)")
-		roomAddress = roomAddress + 2
-
-		while roomSize > 0 do
-
-			-- format item to byte
-			FormatMemory({StartAddress = roomAddress, DataType = EDataType.Byte, DisplayType = EDataItemDisplayType.Decimal})
-			SetDataItemComment(roomAddress, "item number")
-			local roomItem = ReadByte(roomAddress)
-			roomAddress = roomAddress + 1
-
-			-- format item to 16 bit number
-			FormatMemory({StartAddress = roomAddress, DataType = EDataType.Word, NoItems = 2, DisplayType = EDataItemDisplayType.Hex})
-			SetDataItemComment(roomAddress, "char map offset")
-			local itemPosition = ReadWord(roomAddress)
-			roomAddress = roomAddress + 2
-
-			roomSize = roomSize - 3
-		end
-
-		roomNo = roomNo + 1
-	end
-
-end
-
-function FormatRoomItemMemory()
+function GetRoomInstanceDef(instanceNo)
 
 	local instanceAddress = globals.RoomInstances
-	local instanceNo = 0
+	local currInstanceNo = 0
 
-	while instanceNo < 20 do
+	while currInstanceNo < 256 do
 
 		local instanceSize = ReadWord(instanceAddress)
-
-		AddCommentBlock(instanceAddress, "Room Instance " .. tostring(instanceNo) .. " Definition")
-		FormatMemory({StartAddress = instanceAddress, DataType = EDataType.Word, ItemSize = 2, DisplayType = EDataItemDisplayType.Decimal})
-		SetDataItemComment(instanceAddress, "Instance size (".. tostring(instanceSize) .. " bytes)")
-		instanceAddress = instanceAddress + 2
-
-		local charLength = 0
-		local charStartAddress = 0
-
-		while instanceSize > 0 do
-
-			local val = ReadByte(instanceAddress)
-			instanceAddress = instanceAddress + 1
-			instanceSize = instanceSize - 1
-	
-			if val == 0 then
-
-				-- format character map	
-				FormatMemoryAsCharMap(charStartAddress,charLength,1,globals.BackgroundCharacters)
-				charLength = 0
-				charStartAddress = 0
-
-				-- TODO: format gap
-				FormatMemory({StartAddress = instanceAddress-1, DataType = EDataType.ByteArray, DisplayType = EDataItemDisplayType.Decimal, ItemSize = 2})
-				local gapLength = ReadByte(instanceAddress)
-				SetDataItemComment(instanceAddress-1, "space " .. tostring(gapLength) .. " chars")
-
-				instanceAddress = instanceAddress + 1
-				instanceSize = instanceSize - 1
-	
-			else
-				if charLength == 0 then
-					charStartAddress = instanceAddress - 1
-				end
-	
-				charLength = charLength + 1
-			end
-		end
-
-		-- do last chars
-		if charLength > 0 then
-			FormatMemoryAsCharMap(charStartAddress,charLength,1,globals.BackgroundCharacters)
-		end
-
-		instanceNo = instanceNo + 1
 		
-	end
+		if currInstanceNo == instanceNo then
+			return instanceAddress
+		end
 
+		currInstanceNo = currInstanceNo + 1
+		instanceAddress = instanceAddress + instanceSize + 2
+	end
+end
+
+function GetRoomDef(roomNo)
+
+	local roomAddress = globals.RoomDefinitions
+	local currRoomNo = 0
+
+	while currRoomNo < 256 do
+
+		local roomSize = ReadWord(roomAddress)
+		
+		if currRoomNo == roomNo then
+			return roomAddress
+		end
+
+		currRoomNo = currRoomNo + 1
+		roomAddress = roomAddress + roomSize + 2
+	end
+end
+
+function DrawRoomInstanceAt(graphicsView, instanceNo,x,y)
+	
+	local instanceAddr = GetRoomInstanceDef(instanceNo)
+end
+
+function DrawRoom(graphicsView, roomNo, x, y)
+
+	local roomAddress = GetRoomDef(roomNo)
+	local roomSize = ReadWord(roomAddress)
+	roomAddress = roomAddress + 2
+
+	imgui.Text(string.format("room size %d",roomSize))
+	
+	while roomSize > 0 do
+
+		local instanceNo = ReadByte(roomAddress)
+		local itemPosition = ReadWord(roomAddress + 1)
+		local charOffset = itemPosition % 768
+		local posX = charOffset % 32
+		local posY = charOffset // 32
+		
+		imgui.Text(string.format("instance %d position %d,%d",instanceNo,posX,posY))
+		-- TODO: Determine position
+		-- some of the position bits are used as flags
+		DrawRoomInstanceAt(graphicsView,instanceNo,posX,posY)
+
+		-- go to next instance placement
+		roomAddress = roomAddress + 3
+		roomSize = roomSize - 3
+	end
 end
 
 DanDareViewer = 
@@ -98,14 +73,29 @@ DanDareViewer =
     name = "Dan Dare Viewer",  -- rename
 
     -- add your own viewer members here
-    
+	roomNo = 14,
+	
     onAdd = function(self)
-        -- gets called when view is added
-    end,
+		self.graphicsView = CreateZXGraphicsView(256,256)
+        ClearGraphicsView(self.graphicsView, 0)
+		--DrawRoom(self.graphicsView,self.roomNo, 0, 0)
+	end,
 
     onDrawUI = function(self)
         -- gets called every frame
-        imgui.Text("This viewer is called: " .. tostring(self.name))
+        imgui.Text(string.format("This viewer is called: %s",self.name))
+
+		local changed = false
+
+		changed, self.roomNo = imgui.InputInt("room number",self.roomNo)
+
+		--if changed == true then
+			ClearGraphicsView(self.graphicsView, 0)
+			DrawRoom(self.graphicsView,self.roomNo, 0, 0)
+		--end
+
+		-- Update and draw to screen
+		DrawGraphicsView(self.graphicsView)
 
 		if imgui.Button("Format Room Memory") == true then
 			FormatRoomMemory();			
