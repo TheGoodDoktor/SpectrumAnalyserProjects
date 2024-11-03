fa1f1aeb41b1d8c60bdf872d423f5b12e873115e
Sam Hall <sam@colourclash.co.uk>
2024-11-03 10:00:32 +0000

    Auf monty. Figuring out enemy defs

--- aufmonty/Viewer.lua
+++ aufmonty/Viewer.lua
@@ -5,12 +5,17 @@ function CommentRoomData()
 	local roomIndex = 0
 	local curDataPtr = globals.RoomData
 
+	if curDataPtr == nil then
+		print("Could not get enemy defs address from globals.RoomData")
+		return
+	end
+
 	while roomIndex < 79 do
 		SetDataItemComment(curDataPtr, "Room char set [16 bytes]")
 		SetDataItemComment(curDataPtr + 15, "Char set attributes [16 bytes]")
 		SetDataItemComment(curDataPtr + 30, "Room char map data")
 		AddCommentBlock(curDataPtr, "Room " .. tostring(roomIndex))
-		print("Room " .. tostring(roomIndex) .. " address " .. tostring(curDataPtr))
+		print(string.format("Room %d. Address 0x%x ", roomIndex, curDataPtr))
 		repeat 
 			local dataByte = ReadByte(curDataPtr)
 			curDataPtr = curDataPtr + 1
@@ -20,25 +25,52 @@ function CommentRoomData()
 	end
 end
 
+function CommentEnemyDefs()
+
+	local roomIndex = 0
+	local curDataPtr = globals.EnemyDefs
+
+	if curDataPtr == nil then
+		print("Could not get enemy defs address from globals.EnemyDefs")
+		return
+	end
+	
+	while roomIndex < 79 do
+		
+		if roomIndex > 0 then
+			AddCommentBlock(curDataPtr, "Room " .. tostring(roomIndex))
+		end
+		SetDataItemComment(curDataPtr, "Enemy 0")
+		SetDataItemComment(curDataPtr + 4, "Enemy 1")
+		SetDataItemComment(curDataPtr + 8, "Enemy 2")
+		SetDataItemComment(curDataPtr + 12, "Enemy 3")
+
+		print(string.format("Room %d. Address 0x%x ", roomIndex, curDataPtr))
+		
+		roomIndex = roomIndex + 1
+		curDataPtr = curDataPtr + 16
+	end
+end
+
 MontyViewer = 
 {
-    name = "Monty Viewer",
+	name = "Monty Viewer",
 
-    -- add your own viewer members here
-    
-    onAdd = function(self)
-        -- gets called when view is added
-    end,
+	-- add your own viewer members here
 
-    onDrawUI = function(self)
-        -- gets called every frame
-        imgui.Text("This viewer is called: " .. tostring(self.name))
+	onAdd = function(self)
+		-- gets called when view is added
+	end,
 
+	onDrawUI = function(self)
 		if imgui.Button("Comment room data") then
 			CommentRoomData()
 		end
 
-    end
+		if imgui.Button("Comment enemy defs") then
+			CommentEnemyDefs()
+		end
+	end
 }
 
 -- add viewer when file gets loaded
