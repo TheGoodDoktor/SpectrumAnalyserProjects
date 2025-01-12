13560db65786404683b8887c2e0db7bf407230cc
TheGoodDoktor <TheGoodDoktor@users.noreply.github.com>
2025-01-12 15:20:26 +0000

    Work for offscreen buffers

---
+++ Ranarama/OffscreenBuffers.lua
@@ -0,0 +1,24 @@
+OffscreenBufferHandler = {}
+
+function OffscreenBufferHandler.Draw(graphicsView, address, xSize, ySize)
+    -- Draw logic here
+	local drawWidth = ReadByte(globals.ActiveRoomWidthChars) * 8
+	local drawHeight = ReadByte(globals.ActiveRoomHeight)
+    for y = 0, drawHeight - 1 do
+        for x = 0, drawWidth - 1, 8 do
+            
+			DrawPixelLineHeatmap(graphicsView, address, x, y)
+            --state:AdvanceAddressRef(address, 1)
+            address = address + 1
+        end
+    end
+
+end
+
+function OffscreenBufferHandler:GetAddressOffsetFromPos(x, y, xSize, ySize)
+    -- Calculate address offset from position (x, y) here
+    local xSizeChars = xSize >> 3;
+
+	return (x / 8) + (y * xSizeChars);
+end
+
