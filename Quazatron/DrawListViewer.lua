76d967f75eb17eea992df4067d1f5de94ee4603b
markcraig_lucidgames <mark@lucidgames.co.uk>
2025-07-08 11:54:39 +0100

    Update DrawListViewer.lua

--- Quazatron/DrawListViewer.lua
+++ Quazatron/DrawListViewer.lua
@@ -1,22 +1,47 @@
--- Basic viewer template
 
--- rename this
-DrawListViewer = 
+function DrawDrawListToView(view,drawListAddress)
+
+	local noDrawListItems = ReadByte(drawListAddress)
+	drawListAddress = drawListAddress + 1
+
+	local yPos = 0
+	local xPos = 0
+
+	for i = 0, noDrawListItems - 1 do
+		local byte1 = ReadByte(drawListAddress)
+		local byte2 = ReadByte(drawListAddress + 1)
+		local byte3 = ReadByte(drawListAddress + 2)
+		local noPixelLines = byte3 & 3F
+		local itemPixels = GetMemPtr(drawListAddress + 3)
+
+		DrawZXBitImageFineY(view, itemPixels, xPos, yPos, 1, noPixelLines)
+
+		drawListAddress = drawListAddress + 4 + noPixelLines
+	end
+end
+
+-- Drawlist viewer
+DrawListViewer = ZXViewerBase:new( 
 {
-    name = "DrawList Viewer",  -- rename
+	name = "DrawList Viewer",
+	width = 256,
+	height = 256,
+	drawListAddress = 0,
+})
 
-    -- add your own viewer members here
-    
-    onAdd = function(self)
-        -- gets called when view is added
-    end,
+function DrawListViewer:Init()
+	
+	ClearGraphicsView(self.graphicsView, 0)
 
-    onDrawUI = function(self)
-        -- gets called every frame
-        imgui.Text("This viewer is called: " .. tostring(self.name))
+end
 
-    end
-}
+function DrawListViewer:Update()
+
+end
+
+function DrawListViewer:DrawUI()
+
+end
 
 -- add viewer when file gets loaded
 AddViewer(DrawListViewer);
